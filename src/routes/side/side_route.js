05d6b511c835fe4b2e81f288974e26ba52728797
humoyun-webs <humoyuneshpolatov5@gmail.com>
2023-08-29 12:02:23 +0500

    aiuu

--- src/routes/side/side_route.js
+++ src/routes/side/side_route.js
@@ -6,9 +6,9 @@ const {isAuth} = require("../../middlewares/isAuth-middleware")
 const {addSide,Deleteside,EditSide,getside} = require("../../controller/for_admin/side/side")
 
 routes
-.post("/add/side",isAuth, CheckRole('superadmin','direktor'), addSide)
-.delete("/delete/side/:id",isAuth,CheckRole("superadmin","direktor"),Deleteside)
-.put("/edit/side/:id", isAuth,CheckRole("superadmin","direktor"),EditSide)
+.post("/add/side",isAuth, CheckRole('direktor','superadmin'), addSide)
+.delete("/delete/side/:id",isAuth,CheckRole('direktor','superadmin'),Deleteside)
+.put("/edit/side/:id", isAuth,CheckRole('direktor','superadmin'),EditSide)
 .get("/get/side", getside)
 
 module.exports = routes

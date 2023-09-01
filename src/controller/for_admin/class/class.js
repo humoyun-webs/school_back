24e35ded973c8e28f557616b3bf4bf51436b4e27
humoyun-webs <humoyuneshpolatov5@gmail.com>
2023-09-01 20:17:45 +0500

    pushing

--- src/controller/for_admin/class/class.js
+++ src/controller/for_admin/class/class.js
@@ -37,7 +37,40 @@ const ClassCount = async(_, res) =>{
     }
 }
 
+const   Editclass = async ( req, res ) => {
+    try{
+     const { num, lett, descr} = req.body;
+     
+     const {id} = req.params
+     if(!id){
+        return res.status(403).json({message:"Not found"})
+     }
+   
+     const scheme = Joi.object({
+        num:Joi.number().min(1).max(11).required(),
+            lett:Joi.string().max(1).required(),
+            descr:Joi.string().required()
+        
+     });
+   
+     const { error } = scheme.validate({ num, lett, descr});
+   
+     if (error) return res.status(403).json({ message: error.message });
+     
+   
+     const classs = await (
+        num, lett, descr,id
+     );
+   
+     return res.status(201).json({ message: "edit success", newclass: classs });
+    }catch(error){
+   return res.status(404 ).json({message:"Permission denied"})
+//    console.log(error.message);
+    }
+   }
+
 module.exports = {
     AddClass,
-    ClassCount
+    ClassCount,
+    Editclass
 }
--- src/routes/class/class_route.js
+++ src/routes/class/class_route.js
@@ -3,12 +3,12 @@ const routes = Router()
 const {CheckRole} = require("../../middlewares/check-role-middleware")
 const {isAuth} = require("../../middlewares/isAuth-middleware")
 
-const {AddClass,ClassCount} = require("../../controller/for_admin/class/class")
+const {AddClass,ClassCount,Editclass} = require("../../controller/for_admin/class/class")
 
 routes
 .post('/add/class',isAuth,CheckRole("superadmin"), AddClass)
 .get("/count/classes", ClassCount)
-
+.put("/edit/class/:id", isAuth, CheckRole("superadmin"))
 
 module.exports = 
     routes
